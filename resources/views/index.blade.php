66f1f3d7c60a4c7b0121a0604e7f9dbf8eb865b1
Rio Ardiyansyah <ryox@MacBook-Pro-Rio.local>
2026-06-24 17:55:06 +0700

    Tes Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Samsat DIY Terpadu</title>
+    <title>Samsat DIY Terpadu - BETA VERSION</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,13 @@ Route::get('/about', function () {
     return view('about');
 });
 
+Route::post('/webhook-deploy', function () {
+    shell_exec('/var/www/website_samsat/deploy.sh');
+    return 'Deployed!';
+});
+
+use App\Http\Controllers\KendaraanController;
+
+Route::resource('kendaraan', KendaraanController::class);
+
 ?>
