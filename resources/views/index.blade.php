f361de9ae55bc960aec9cc3443ab8f7d1b0e7fc1
agungfc421-lab <agungfc421@gmail.com>
2026-06-24 19:52:48 +0700

    update dikit yee

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Samsat DIY</title>
-    
-    <script src="https://cdn.tailwindcss.com"></script>
-
-=======
->>>>>>> 02f39d84ffffa7fc00d566a5903e0d95e4ec7ebb
     <title>SAMSAT DIY - Layanan Pajak Kendaraan Digital</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
