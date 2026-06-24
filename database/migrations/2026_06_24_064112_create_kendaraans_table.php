0ad8a996c401dd6588e4d083880cf0769a28b37f
Adilsa2254 <al.saptr4@gmail.com>
2026-06-24 16:06:19 +0700

    DB [1]

--- database/migrations/2026_06_24_064112_create_kendaraans_table.php
+++ database/migrations/2026_06_24_064112_create_kendaraans_table.php
@@ -11,7 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('kendaraans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomor_polisi');
+            $table->string('merek'); 
+            $table->string('tipe'); 
+            $table->integer('tahun_pembuatan');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('kendaraans');
     }
 };
