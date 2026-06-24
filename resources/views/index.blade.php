3b8d91bee46549c311c64e4a28d7728cf982b655
Adilsa2254 <al.saptr4@gmail.com>
2026-06-24 18:09:54 +0700

    ubah beberpa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Samsat DIY Terpadu - BETA VERSION</title>
+    <title>Samsat DIY</title>
+    
     <script src="https://cdn.tailwindcss.com"></script>
 =======
     <title>SAMSAT DIY - Layanan Pajak Kendaraan Digital</title>
--- routes/web.php
+++ routes/web.php
@@ -23,17 +23,15 @@ Route::post('/webhook-deploy', function () {
     return 'Deployed!';
 });
 
-// Kendaraan Resource
+
 Route::resource('kendaraan', KendaraanController::class);
 
-// Authentication routes
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'processLogin'])->name('login.process');
 
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'processRegister'])->name('register.process');
 
-// Protected routes
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
