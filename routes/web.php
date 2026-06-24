7ea7e55f95528e48ed0dfd6574fd317730d0fd91
Adilsa2254 <al.saptr4@gmail.com>
2026-06-24 11:54:25 +0700

    add about

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/faq', function () {
     return view('faq');
 })->name('faq');
+
+Route::get('/About', function () {
+    return view('About');
+})->name('About');
