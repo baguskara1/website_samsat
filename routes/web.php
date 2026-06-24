aa2ad042dde1141014fe0a5792d292cf24817e17
Adilsa2254 <al.saptr4@gmail.com>
2026-06-24 12:10:57 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
-Route::get('/faq', function () {
-    return view('faq');
-})->name('faq');
-
-Route::get('/About', function () {
-    return view('About');
-})->name('About');
